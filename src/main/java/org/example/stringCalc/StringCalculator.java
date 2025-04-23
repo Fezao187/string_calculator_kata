303b5092b60e17a093c7464a5ff62aaba39b26d3
Katlego Maredi <katlegomaredi@172-15-12-154.lightspeed.sntcca.sbcglobal.net>
2025-04-23 14:32:06 +0200

    first commit

---
+++ src/main/java/org/example/stringCalc/StringCalculator.java
@@ -0,0 +1,5 @@
+package org.example.stringCalc;
+
+public class StringCalculator {
+
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,9 @@
+import org.example.stringCalc.StringCalculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringCalculatorTest {
+    StringCalculator sc = new StringCalculator();
+
+}
