c265c25a114e1dd8920eff2a1f334fc30af06454
Fezao187 <katlegomaredi@gmail.com>
2025-04-23 14:48:40 +0200

    Test 1: shouldReturnZeroOnEmptyString

--- src/main/java/org/example/stringCalc/StringCalculator.java
+++ src/main/java/org/example/stringCalc/StringCalculator.java
@@ -1,5 +1,11 @@
 package org.example.stringCalc;
 
 public class StringCalculator {
-
+    public int add(String numbers){
+        if(numbers.isEmpty()){
+            return 0;
+        } else{
+            return -1;
+        }
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -6,4 +6,13 @@ import static org.junit.Assert.assertEquals;
 public class StringCalculatorTest {
     StringCalculator sc = new StringCalculator();
 
+    @Test
+    public void shouldReturnZeroOnEmptyString(){
+        assertEquals(0, sc.add(""));
+    }
+
+    @Test
+    public void shouldReturnOneIfOneIsPassed(){
+        assertEquals(1, sc.add("1"));
+    }
 }
